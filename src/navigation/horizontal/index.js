1fb0c5a541bd3b1a220be962c08d30ea77baa91d
lT <tywin0302@gmail.com>
2021-03-22 11:25:29 +0800

    fix: 恢复测试模板页面

--- src/navigation/horizontal/index.js
+++ src/navigation/horizontal/index.js
@@ -1,13 +1,13 @@
-import apps from './apps'
-import attendance from './attendance'
+import home from './home'
 import training from './training'
+import docCenter from './doc-center'
+import attendance from './attendance'
+import apps from './apps'
 import pages from './pages'
 import chartsAndMaps from './charts-and-maps'
 import uiElements from './ui-elements'
 import formAndTable from './forms-and-table'
 import others from './others'
-import docCenter from './doc-center'
-import home from './home'
 
 // Array of sections
-export default [...home, ...training, ...docCenter, ...attendance]
+export default [...home, ...training, ...docCenter, ...attendance, ...apps, ...pages, ...chartsAndMaps, ...uiElements, ...formAndTable, ...others]
--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,12 +1,12 @@
-import dashboard from './dashboard'
-import appsAndPages from './apps-and-pages'
 import home from './home'
 import docCenter from './doc-center'
 import office from './office'
-import others from './others'
+import dashboard from './dashboard'
+import appsAndPages from './apps-and-pages'
 import chartsAndMaps from './charts-and-maps'
 import uiElements from './ui-elements'
 import formAndTable from './forms-and-table'
+import others from './others'
 
 // Array of sections
-export default [...home, ...docCenter, ...office]
+export default [...home, ...docCenter, ...office, ...appsAndPages, ...chartsAndMaps, ...uiElements, ...formAndTable, ...others]
