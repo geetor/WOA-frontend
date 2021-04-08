7c91684d391c878cdf1dd1ea18c2c9233c373ca6
GeQiao <171250029@smail.nju.edu.cn>
2021-04-08 22:30:28 +0800

    portal标签的问题

--- src/views/management/user/User.vue
+++ src/views/management/user/User.vue
@@ -2,7 +2,7 @@
   <div style="height: inherit">
     <div
       class="body-content-overlay"
-      :class="{'show': mqShallShowLeftSidebar }"
+      :class="{show: mqShallShowLeftSidebar }"
       @click="mqShallShowLeftSidebar = false"
     />
 
