efdb3b63a7b8c8981629f33ad8558016b64ac7a5
Toyama-Kazuha <625339743@qq.com>
2021-05-15 11:10:19 +0800

    fix:修改了文档中心nav导航栏未选中的bug

--- src/router/routes/doc-center.js
+++ src/router/routes/doc-center.js
@@ -4,26 +4,32 @@ export default [
     name:'doc-center-list',
     component: ()=>import('@/views/doc-center/BlogList.vue'),
     meta:{
-      navActiveLink:'doc-center'
+      navActiveLink:'doc-center-list'
     }
   },
   {
     path:'/doc-center/edit',
     name:'doc-center-edit',
-    component:()=>import('@/views/doc-center/BlogEdit.vue')
+    component:()=>import('@/views/doc-center/BlogEdit.vue'),
+    meta:{
+      navActiveLink:'doc-center-list'
+    }
   },
   {
     path: '/doc-center/detail/:docId',
     name:'doc-center-detail',
     component:()=>import('@/views/doc-center/BlogDetail.vue'),
     meta:{
-      navActiveLink:'doc-center'
+      navActiveLink:'doc-center-list'
     }
   },
   {
     path:'/doc-center/category/:category',
     name:'doc-center-category',
-    component:()=>import('@/views/doc-center/BlogCategory.vue')
+    component:()=>import('@/views/doc-center/BlogCategory.vue'),
+    meta:{
+      navActiveLink:'doc-center-list'
+    }
   }
 
 ]
