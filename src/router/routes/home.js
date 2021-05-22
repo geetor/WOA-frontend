39853add976d7abafada8e35f1c1c19fc47f95b3
lT <tywin0302@gmail.com>
2021-05-22 13:48:58 +0800

    chore: 修改首页模块的目录结构

--- src/router/routes/home.js
+++ src/router/routes/home.js
@@ -2,43 +2,32 @@ export default [
     {
         path: '/',
         name: 'home-homepage',
-        component: () => import('@/views/home/homepage.vue'),
+        component: () => import('@/views/home/Homepage.vue'),
     },
-
-    ///////////////////////////////////////
     {
         path: '/bulletins',
         name: 'bulletin-page',
-        component: () => import('@/views/home/bulletinDetail/BulletinList.vue'),
+        component: () => import('@/views/home/bulletin-detail/BulletinList.vue'),
         meta: {
             navActiveLink: 'home-homepage',
             contentRenderer: 'sidebar-left',
-            contentClass: 'training-application',
+            contentClass: 'training-application'
         }
     },
-    // {
-    //     path: '/bulletins/preview',
-    //     name: 'bulletin-preview',
-    //     component: () => import('@/views/home/bulletinDetail/BulletinPreview.vue'),
-    //     meta: {
-    //         navActiveLink: 'home-homepage',
-    //     }
-    // },
     {
         path: '/bulletins/preview',
         name: 'bulletin-preview',
-        component: () => import('@/views/home/bulletinDetail/BulletinDetailCard.vue'),
+        component: () => import('@/views/home/bulletin-detail/BulletinDetailCard.vue'),
         meta: {
-            navActiveLink: 'home-homepage',
+            navActiveLink: 'home-homepage'
         }
     },
     {
         path: '/bulletins/edit',
         name: 'bulletin-edit',
-        component: () => import('@/views/home/bulletinDetail/BulletinEdit.vue'),
+        component: () => import('@/views/home/bulletin-detail/BulletinEdit.vue'),
         meta: {
-            navActiveLink: 'home-homepage',
+            navActiveLink: 'home-homepage'
         }
-    },
-
+    }
 ]
